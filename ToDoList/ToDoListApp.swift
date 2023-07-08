8850ea9c0c69f21b3226d993f5b6596d38f3d76f
bscobie <benscobie24@gmail.com>
2023-07-08 18:12:26 +1200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ben Scobie on 8/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
