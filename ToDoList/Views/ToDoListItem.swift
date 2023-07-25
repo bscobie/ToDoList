60ab4ee9e5365bdba4a84f6d2dff76cabbea25f2
bscobie <benscobie24@gmail.com>
2023-07-25 16:33:18 +1200

    Added empty view files for each screen the app should have.

---
+++ ToDoList/Views/ToDoListItem.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListItem.swift
+//  ToDoList
+//
+//  Created by Ben Scobie on 25/07/23.
+//
+
+import SwiftUI
+
+struct ToDoListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListItem()
+    }
+}
