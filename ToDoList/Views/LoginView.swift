70bf37703869d2f46a64e0533ee862776512fced
bscobie <benscobie24@gmail.com>
2023-07-25 17:40:46 +1200

    Created basic form for logging in and button for new users to register.

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView()
             
             // Login Form
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button {
+                    // Attempt log in
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log In")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                }
+            }
             
             // Create Account
+            VStack {
+                Text("New around here?")
+                Button("Create An Account") {
+                    // Show registration
+                }
+            }
             
             Spacer()
         }
