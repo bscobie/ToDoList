e979817b5dd6ce7685989c08fcb2f11b41a5291f
bscobie <benscobie24@gmail.com>
2023-07-25 16:54:13 +1200

    Abstracted header into its own view file. Tidies up code up a bit and now can get called across different views easier.

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -11,26 +11,7 @@ struct LoginView: View {
     var body: some View {
         VStack {
             // Header
-            ZStack {
-                RoundedRectangle(cornerRadius: 0)
-                    .foregroundColor(Color.pink)
-                    .rotationEffect(Angle(degrees: 15))
-                
-                VStack {
-                    Text("To Do List")
-                        .font(.system(size:50))
-                        .foregroundColor(Color.white)
-                        .bold()
-                    Text("Get things done")
-                        .font(.system(size: 30))
-                        .foregroundColor(Color.white)
-
-                }
-                .padding(.top, 30)
-            }
-            .frame(width: UIScreen.main.bounds.width * 3, height: 300)
-            .offset(y: -100)
-
+            HeaderView()
             // Login Form
             
             // Create Account
