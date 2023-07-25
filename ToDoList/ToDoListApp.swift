fdf213446446adc58d07af622d917db4580a7768
bscobie <benscobie24@gmail.com>
2023-07-25 16:19:20 +1200

    Add firebase import.

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -5,10 +5,16 @@
 //  Created by Ben Scobie on 8/07/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
