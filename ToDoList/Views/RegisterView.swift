f3c2a43442a105d56767defe81c17cf1eaf06d40
bscobie <benscobie24@gmail.com>
2023-07-31 18:55:46 +1200

    Created form for a new user to register.

--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView(title: "Register", subtitle: "Start organising your todos", angle: -15, background: .green)
+            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                TextField("Email Adress", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+            }
+            
             Spacer()
         }
     }
