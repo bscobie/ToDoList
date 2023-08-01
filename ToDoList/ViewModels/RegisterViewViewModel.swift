0000c56b83ddd6443a4fe28ccd8087d2035ba4dd
bscobie <benscobie24@gmail.com>
2023-08-01 19:53:08 +1200

    Started implementation of account registration functionality.

--- ToDoList/ViewModels/RegisterViewViewModel.swift
+++ ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class RegisterViewViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    init() {}
+    
+    func register() {
+        
+    }
+    
+    private func validate() {
+        
+    }
+}
