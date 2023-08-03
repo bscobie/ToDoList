1cd9455dd30584f7375e675337eb466faf68e379
bscobie <benscobie24@gmail.com>
2023-08-03 20:15:04 +1200

    Added some padding to text.

--- ToDoList/Views/NewItemView.swift
+++ ToDoList/Views/NewItemView.swift
@@ -15,6 +15,7 @@ struct NewItemView: View {
             Text("New Item")
                 .font(.system(size: 32))
                 .bold()
+                .padding(.top, 70)
             
             Form {
                 TextField("Title", text: $viewModel.title)
