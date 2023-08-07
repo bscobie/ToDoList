f426bb15a710aefb3946fafdeb985df7524baa4e
bscobie <benscobie24@gmail.com>
2023-08-07 20:06:40 +1200

    Showing item passed in through preview.

--- ToDoList/Views/ToDoListItemView.swift
+++ ToDoList/Views/ToDoListItemView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct ToDoListItemView: View {
+    
+    let item: ToDoListItem
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack {
+                Text(item.title)
+                Text("\(Date(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated, time: .shortened))")
+            }
+            
+            Spacer()
+        }
     }
 }
 
 struct ToDoListItemView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListItemView()
+        ToDoListItemView(item: .init(id: "123", title: "get milk", dueDate: Date().timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false))
     }
 }
